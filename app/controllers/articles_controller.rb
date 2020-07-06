2fabee3616ef644134ff042e16531134413e15f5
Karen Ho <karenho008@gmail.com>
2020-07-05 23:55:05 -0400

    Add form to create new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,8 +2,22 @@ class ArticlesController < ApplicationController
     def index 
         @articles = Article.all
     end
-    
+
     def show
         @article = Article.find(params[:id])
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] = "Article was created successfully!"
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+
+    def new
+        @article = Article.new
     end
 end
