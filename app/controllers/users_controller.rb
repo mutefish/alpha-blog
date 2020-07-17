0a443a71facf88570eb601c0de88694dc7804164
Karen Ho <karenho008@gmail.com>
2020-07-16 23:30:10 -0400

    Add user details page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,11 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+
+    def show
+        @user = User.find(params[:id])
+        @articles = @user.articles
+    end
     
     def create
         @user = User.new(user_params)
