ae6ceda9bc93d783eb1c76477662ddd3925f7d37
Karen Ho <karenho008@gmail.com>
2020-07-20 21:43:48 -0400

    Use session data to show proper links in navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update]
     def new
         @user = User.new
     end
@@ -8,13 +9,13 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id])
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
     
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome #{@user.username}! You signed up successfully!"
             redirect_to articles_path
         else
@@ -23,11 +24,9 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
     end
 
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your account information has been updated!"
             redirect_to @user
@@ -40,4 +39,8 @@ class UsersController < ApplicationController
     def user_params 
         params.require(:user).permit(:username, :email, :password)
     end
+
+    def set_user
+        @user = User.find(params[:id])
+    end 
 end
